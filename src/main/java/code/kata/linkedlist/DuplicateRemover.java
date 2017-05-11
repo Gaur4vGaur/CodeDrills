d327ee3ffb24015c2eced0ba0d5287a74660f8b7
Gaurav Gaur <vaibgaurav@gmail.com>
2017-05-11 06:46:51 +0100

    started with duplicate remover in linkedlist

---
+++ src/main/java/code/kata/linkedlist/DuplicateRemover.java
@@ -0,0 +1,18 @@
+package code.kata.linkedlist;
+
+import java.util.List;
+import java.util.LinkedList;
+
+/**
+ * The class removes duplicates from an unsorted linked list
+ *
+ * @author: Gaurav Gaur
+ *
+ **/
+
+public class DuplicateRemover {
+
+    public static <T> List<T> removeDuplicates(List<T> input) {
+        return null;
+    }
+}
