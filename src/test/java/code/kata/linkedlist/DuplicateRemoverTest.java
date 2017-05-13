4ccc3c016473a5bbc9cac6ec51fb1fc85d74e7bf
Gaurav Gaur <vaibgaurav@gmail.com>
2017-05-13 23:21:50 +0100

    added test cases for Duplicate Remover

--- src/test/java/code/kata/linkedlist/DuplicateRemoverTest.java
+++ src/test/java/code/kata/linkedlist/DuplicateRemoverTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import java.util.List;
 import java.util.LinkedList;
 import static code.kata.util.Printer.pln;
+import static org.junit.Assert.assertEquals;
 
 /**
  * The class removes duplicates from an unsorted linked list
@@ -13,10 +14,20 @@ import static code.kata.util.Printer.pln;
  **/
 
 public class DuplicateRemoverTest {
+    List<Integer> list = null;
+
+    private List<Integer> fetchResult(Integer... integers) {
+        List<Integer> result = new LinkedList<>();
+        for(Integer integer: integers) {
+            result.add(integer);
+        }
+
+        return result;
+    }
 
     @Test
-    public void testDuplicates() {
-        List<Integer> list = new LinkedList<Integer>();
+    public void testDuplicatesWithAllSameElement() {
+        list = new LinkedList<>();
 
         list.add(1);
         list.add(1);
@@ -25,11 +36,37 @@ public class DuplicateRemoverTest {
         list.add(1);
 
         DuplicateRemover.removeDuplicates(list);
+        assertEquals("size of result shoudl be 1", list.size(), 1);
+        assertEquals("elemnts are not equal", list, fetchResult(1));
+    }
 
-        for(Integer ele : list) {
-            pln(ele);
-        }
+    @Test
+    public void testDuplicatesWithMultipleDuplicates() {
+        list = new LinkedList<>();
 
+        list.add(1);
+        list.add(1);
+        list.add(2);
+        list.add(2);
+        list.add(3);
+
+        DuplicateRemover.removeDuplicates(list);
+        assertEquals("size of result shoudl be 1", list.size(), 3);
+        assertEquals("elemnts are not equal", list, fetchResult(1,2,3));
     }
     
+    @Test
+    public void testDuplicatesWithDistinctElements() {
+        list = new LinkedList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+        DuplicateRemover.removeDuplicates(list);
+        assertEquals("size of result shoudl be 1", list.size(), 4);
+        assertEquals("elemnts are not equal", list, fetchResult(1,2,3,4));
+
+    }
 }
