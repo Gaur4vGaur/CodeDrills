a45c54d8bb22411f75588279265e0884717f6aec
gaurav.gaur <gaurav.gaur@digital.hmrc.gov.uk>
2017-12-19 16:07:29 +0000

    Test

--- src/test/java/ch1/IsPermutationTest.java
+++ src/test/java/ch1/IsPermutationTest.java
@@ -6,43 +6,43 @@ import static org.junit.Assert.assertFalse;
 
 public class IsPermutationTest {
 
-	@Test
-	public void whenStringIsPermutation() {
-		assertTrue("Gaurav is permutation of varuaG", IsPermutation.isPermutation("Gaurav", "vauraG"));
-	}
+    @Test
+    public void whenStringIsPermutation() {
+        assertTrue("Gaurav is permutation of varuaG", IsPermutation.isPermutation("Gaurav", "vauraG"));
+    }
 
-	@Test
-	public void whenStringIsNotPermutation() {
-		assertFalse("Gaurav is not permutation of varuav", IsPermutation.isPermutation("Gaurav", "varuav"));
-	}
+    @Test
+    public void whenStringIsNotPermutation() {
+        assertFalse("Gaurav is not permutation of varuav", IsPermutation.isPermutation("Gaurav", "varuav"));
+    }
 
-	@Test
-	public void whenStringIsHavingExtraSpaces() {
-		assertFalse("Gaurav is not permutation of varuav", IsPermutation.isPermutation("Gaurav", "Gaurav "));
-	}
+    @Test
+    public void whenStringIsHavingExtraSpaces() {
+        assertFalse("Gaurav is not permutation of varuav", IsPermutation.isPermutation("Gaurav", "Gaurav "));
+    }
     
 }
 
 class IsPermutation {
 
-	public static boolean isPermutation(String source, String testString) {
-		int[] tracker = new int[128];
+    public static boolean isPermutation(String source, String testString) {
+        int[] tracker = new int[128];
 
-		if(source.length() != testString.length()) {
-			return false;
-		} else {
-			for(char s: source.toCharArray()) {				
-				tracker[s] = tracker[s]+1;
-			}
+        if(source.length() != testString.length()) {
+            return false;
+        } else {
+            for(char s: source.toCharArray()) {             
+                tracker[s] = tracker[s]+1;
+            }
 
-			for(char s: testString.toCharArray()) {
-				tracker[s] = tracker[s]-1;
-				if(tracker[s] < 0)
-					return false;
-			}
+            for(char s: testString.toCharArray()) {
+                tracker[s] = tracker[s]-1;
+                if(tracker[s] < 0)
+                    return false;
+            }
 
-			return true;
-		}
-	}
+            return true;
+        }
+    }
 
 }
